2a157735b0f89bafa4d1edcbc1729cc9c178ff9a
Leonard Lin <gwokae@gmail.com>
2019-04-29 22:16:23 +0800

    add table body add init data & config

--- src/index.js
+++ src/index.js
@@ -9,8 +9,25 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      data: [],
-      config: {},
+      data: [
+        {
+          id: 1,
+          name: 'John Doe',
+          price: 123,
+          notes: 'Hello World',
+        },
+      ],
+      config: {
+        fields: [
+          { key: 'name', type: 'text' },
+          { key: 'price', type: 'number' },
+          { key: 'notes', type: 'textarea' },
+        ],
+        actions: [
+          { name: 'Edit' },
+          { name: 'Delete' },
+        ],
+      },
     };
   }
 
@@ -19,6 +36,8 @@ class App extends React.Component {
     return (
       <React.Fragment>
         <h1>dailydrinks-assignment</h1>
+        <button type="button">Add order</button>
+        <h2>Orders:</h2>
         <Table config={config} data={data} />
       </React.Fragment>
     );
--- src/table/Table.js
+++ src/table/Table.js
@@ -3,13 +3,54 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Table extends React.Component {
-  static propsType = {
+  static propTypes = {
     config: PropTypes.objectOf(() => (true)).isRequired,
     data: PropTypes.arrayOf(() => (true)).isRequired,
   }
 
+  getTableHead() {
+    const { config: { fields } } = this.props;
+    return (
+      <thead>
+        <tr>
+          {fields.map(({ key, label = key }) => (<th key={key}>{label}</th>))}
+          <th>Actions</th>
+        </tr>
+      </thead>
+    );
+  }
+
+  getTableBody() {
+    const { data } = this.props;
+    return (
+      <tbody>
+        {data.map(item => (this.getTableItem(item)))}
+      </tbody>
+    );
+  }
+
+  getTableItem(data) {
+    const { config: { fields, actions } } = this.props;
+    const { id } = data;
+    return (
+      <tr key={id}>
+        {fields.map(({ key }) => (
+          <td key={`data-${id}-${key}`}>{data[key]}</td>
+        ))}
+        <td>
+          {actions.map(({ name, handler }) => (<button type="button" onClick={handler}>{name}</button>))}
+        </td>
+      </tr>
+    );
+  }
+
   render() {
-    return (<h1>hello</h1>);
+    return (
+      <table>
+        { this.getTableHead() }
+        { this.getTableBody() }
+      </table>
+    );
   }
 }
 
